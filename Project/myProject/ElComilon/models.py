168e80df8df3bf3d61d04efb414a2dedba2a5fba
CETECOM <CETECOM@AVAWSV01LC0119.duoc.acad>
2024-06-07 10:58:25 -0400

    lol

--- Project/myProject/ElComilon/models.py
+++ Project/myProject/ElComilon/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Producto(models.Model):
+    id_producto = models.Autofield(primary_key = True)
+    nombre = models.CharField(max_length = 30)
+    precio = models.CharField(max_length = 5)
+    descripcion = models.CharField(max_length = 50)
+    ingredientes = models.CharField(max_length = 80)
+    cantidad = models.CharField(max_length = 3)
+    
+    def __str__(self):
+        return str(self.id_producto
+                + " "
+                + str(self.nombre)
+                + " "
+                + str(self.precio)
+                + " "
+                + str(self.cantidad)
+        )
