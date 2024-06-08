3392ea11229e0f9afb6127eff390cfc6cdbc1755
DesertmasterS <164028689+DesertmasterS@users.noreply.github.com>
2024-06-08 19:30:50 -0400

    perfil

--- Project/myProject/ElComilon/urls.py
+++ Project/myProject/ElComilon/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('Register',views.register, name='Register'),
     path('Login', views.login, name='Login'),
-    path('detalleCompra', views.detalleCompra, name='detalleCompra')
+    path('detalleCompra', views.detalleCompra, name='detalleCompra'),
+    path('Profile',views.Profile, name='Profile')
 ]
