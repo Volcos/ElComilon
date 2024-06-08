2c160e546fef2abc25ed978ea38356cd7aeded07
vent30491 <vargasbenjamin847@gmail.com>
2024-06-07 20:35:18 -0400

    Details

--- Project/myProject/ElComilon/models.py
+++ Project/myProject/ElComilon/models.py
@@ -4,19 +4,13 @@ from django.db import models
 
 
 class Producto(models.Model):
-    id_producto = models.Autofield(primary_key = True)
-    nombre = models.CharField(max_length = 30)
-    precio = models.CharField(max_length = 5)
+    id_producto = models.IntegerField(primary_key = True)
+    nombre = models.CharField(max_length = 80)
+    precio = models.IntegerField()
     descripcion = models.CharField(max_length = 50)
     ingredientes = models.CharField(max_length = 80)
-    cantidad = models.CharField(max_length = 3)
+    imagen = models.CharField(max_length=300)
     
-    def __str__(self):
-        return str(self.id_producto
-                + " "
-                + str(self.nombre)
-                + " "
-                + str(self.precio)
-                + " "
-                + str(self.cantidad)
-        )
+    
+class Test(models.Model):
+    nombre = models.CharField(max_length=30)
--- Project/myProject/ElComilon/views.py
+++ Project/myProject/ElComilon/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Producto
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    productos = Producto.objects.all()
+    return render(request, 'pages/index.html', {'productos':productos})
 
 def register(request):
     return render(request, 'pages/Register.html')
